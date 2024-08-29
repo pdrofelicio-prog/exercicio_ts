8fb4a43338f42e432c744ea6b4d9312ddc796f02
pdrofelicio-prog <pedro.felicio.silva@gmail.com>
2024-08-28 23:32:40 -0300

    Add files via upload

---
+++ olá maria.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(5, 3); // Retorna 15
+const saudacaoNome = saudacao("Maria"); // Retorna "Olá Maria"
+
+console.log(resultadoMultiplicacao); // Saída: 15
+console.log(saudacaoNome); // Saída: Olá Maria
